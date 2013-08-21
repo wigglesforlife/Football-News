3bd1eb5a58513496eb884faacfb529aa2b5e111b
Dave Morris <davey.morris.dm@googlemail.com>
2013-08-21 20:15:03 +0100

    Created a parser that checks if url has already been parsed rather than blindly parsing all urls

---
+++ app/classes/Dmorris/Parser/KindParser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Dmorris\Parser;
+
+use Sunra\PhpSimple\HtmlDomParser;
+use Illuminate\Support\Facades\Cache;
+use Dmorris\Parser\Parser;
+
+/**
+* Similar to ordinary parser, but doesn't blindly parse every link. If a link has already been parsed it will be skipped.
+*/
+abstract class KindParser extends Parser
+{
+	protected $ignoreParse = false;
+
+	public function __construct($url = null, $keywords = array())
+	{
+		if(!$this->checkUrl($url))
+		{
+			parent::__construct($url, $keywords);
+		}
+			
+		else
+		{
+			$this->ignoreParse = true;
+			$this->url = $url;
+			$this->keywords = $keywords;
+		}
+			
+	}
+
+	public function checkUrl($url = null)
+	{
+		is_null($url) AND $url = $this->url;
+		return \Story::where('hash','=',md5($url))->count() > 0;
+	}
+
+	// Stub. We don't want the destructor doing memory clean up here for the domparser
+	public function __destruct() {}
+}
